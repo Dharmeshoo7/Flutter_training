636832aaa90240667aed3dd13f2ecc0ba1ebb522
Dharmeshoo7 <cdharmesh.gajera@gmail.com>
2023-11-09 03:56:04 -0800

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_training/models/catalog.dart';
 import 'package:flutter_training/widgets/drawer.dart';
 import 'package:flutter_training/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     final days = 20;
     final name = "Codepur";
-    final dummyList = List.generate(30, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(30, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
@@ -21,14 +46,18 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items!.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CatalogModel.items![index],
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -8,19 +8,22 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      tileColor: Colors.lightBlue.shade50,
-      onTap: () {
-        print("${item.name} Pressed");
-      },
-      leading: Image.network(
-          "https://imageflix.cam/images/2023/05/31/5898ec7bbfa0cda33adcf212ed5ba92a.md.jpg"),
-      title: Text(item.name),
-      subtitle: Text(item.desc),
-      trailing: Text(
-        "\$${item.price}",
-        textScaleFactor: 1.5,
-        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+    return Card(
+      elevation: 0.0,
+      child: ListTile(
+        // tileColor: Colors.lightBlue.shade50,
+        onTap: () {
+          print("${item.name} Pressed");
+        },
+        leading: Image.network(
+            "https://imageflix.cam/images/2023/05/31/5898ec7bbfa0cda33adcf212ed5ba92a.md.jpg"),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price}",
+          textScaleFactor: 1.5,
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
